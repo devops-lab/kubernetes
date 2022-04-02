d7d3fc785597c4ada4e941927238da0e1f6ea3ce
Renatas Vencevicius <vencrena@gmail.com>
2022-04-02 15:33:29 +0300

    vagrant files updates

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -55,7 +55,7 @@ echo "export TERM=xterm" >> /etc/bash.bashrc
 
 echo "[TASK 10] Update /etc/hosts file"
 cat >>/etc/hosts<<EOF
-172.16.16.100   kmaster.example.com     kmaster
-172.16.16.101   kworker1.example.com    kworker1
-172.16.16.102   kworker2.example.com    kworker2
+172.16.16.100   kmaster.lab.local     kmaster
+172.16.16.101   kworker1.lab.local    kworker1
+172.16.16.102   kworker2.lab.local    kworker2
 EOF
--- vagrant/bootstrap_kworker.sh
+++ vagrant/bootstrap_kworker.sh
@@ -2,5 +2,5 @@
 
 echo "[TASK 1] Join node to Kubernetes Cluster"
 apt install -qq -y sshpass >/dev/null 2>&1
-sshpass -p "kubeadmin" scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no kmaster.example.com:/joincluster.sh /joincluster.sh 2>/dev/null
+sshpass -p "kubeadmin" scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no kmaster.lab.local:/joincluster.sh /joincluster.sh 2>/dev/null
 bash /joincluster.sh >/dev/null 2>&1
